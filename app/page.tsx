d9b064d98c8f494d94136eebf94d52d94ad4387d
sam new lap <sam@gmail.com>
2024-04-11 18:46:10 +0530

    Marketing Page

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <h1 className="text-sky-500">Hello Trello</h1>;
-}
--- app/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-const IdPage = ({ params }: { params: { id: string } }) => {
-  return (
-    <div>
-      <p>ID : {params.id}</p>
-    </div>
-  );
-};
-
-export default IdPage;
